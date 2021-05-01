7d6f7f00c551daee1965899840811ef6fee30c23
Prahlad Nayak <pnayak1501@gmail.com>
2021-05-01 13:25:50 +0530

    Create swapping_twonos.cpp

---
+++ functions/swapping_twonos.cpp
@@ -0,0 +1,38 @@
+// Write a program with a function to swap the values of 2 given integer variables
+
+#include<iostream>
+using namespace std;
+void wrong_swap(int a, int b){
+    int temp;
+    temp = a;
+    a = b;
+    b = temp;
+}
+
+void correct_swap(int *a, int *b){
+    int temp;
+    temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+
+int main(){
+ int a,b;
+ cin>>a>>b;
+ cout<<"Before swap: "<<endl;
+ cout<<"a= "<<a<<endl;
+ cout<<"b= "<<b<<endl; 
+ wrong_swap(a,b);
+ cout<<"After swap: "<<endl;
+ cout<<"a= "<<a<<endl;
+ cout<<"b= "<<b<<endl;
+ cout<<"Before swap: "<<endl;
+ cout<<"a= "<<a<<endl;
+ cout<<"b= "<<b<<endl; 
+ correct_swap(&a,&b);
+ cout<<"After swap: "<<endl;
+ cout<<"a= "<<a<<endl;
+ cout<<"b= "<<b<<endl;
+ return 0;
+}

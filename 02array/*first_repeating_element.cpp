3647f6ea1a9a789cb2d09f053b7f440c7ab6ecf1
Prahlad Nayak <pnayak1501@gmail.com>
2021-05-03 20:13:49 +0530

    Create *first_repeating_element.cpp

---
+++ 02array/*first_repeating_element.cpp
@@ -0,0 +1,38 @@
+/* Given an array arr[] of size N. The task is to find the first repeating element in an array of integers, i.e., an element that occurs more than once and whose index of first occurrence is smallest.
+*/
+
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin>>n;
+    
+    int a[n];
+    for(int i=0;i<n;i++){
+        cin>>a[i];
+    }
+    
+    const int N = 1e6+2;
+    int idx[N];
+    for(int i=0;i<N;i++){
+        idx[i]=-1;
+    }
+    int minindx = INT_MAX;
+    
+    for(int i=0;i<n;i++){
+        if(idx[a[i]]!=-1){
+            minindx = min(minindx, idx[a[i]]);
+        }
+        else{
+            idx[a[i]] = i;
+        }
+    }
+    if(minindx == INT_MAX)
+        cout<<"-1"<<endl;
+    else
+        cout<<minindx+1<<endl;
+    
+    return 0;
+}

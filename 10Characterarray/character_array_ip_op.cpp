b580dc68dc091ce5f79490139646158411d2d31c
Prahlad Nayak <pnayak1501@gmail.com>
2021-05-04 14:34:49 +0530

    Create character_array_ip_op.cpp

---
+++ 10Characterarray/character_array_ip_op.cpp
@@ -0,0 +1,15 @@
+// Character i/p o/p
+
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+    char arr[100];
+    cin>>arr;
+
+    cout<<arr;
+
+    return 0;
+}

d64e2689cfa2be241013ae23993474235fb28b38
Prahlad Nayak <pnayak1501@gmail.com>
2021-05-04 17:32:10 +0530

    Create strings_basics.cpp

---
+++ 13strings/strings_basics.cpp
@@ -0,0 +1,26 @@
+// String basics
+
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+    // Different ways to declare a string
+
+    // string str;
+    // // cin>>str;
+    // // cout<<str<<endl;
+
+    // string str1(5, 'n');
+    // cout<<str1<<endl;
+
+    // string str2 = "Prahlad";
+    // cout<<str2;
+
+    // string str3;
+    // getline(cin,str3);
+    // cout<<str3<<endl;
+
+    return 0;
+}

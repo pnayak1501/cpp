362dfa07f9b6009a674be26bc3f16da2691020c3
Prahlad Nayak <pnayak1501@gmail.com>
2021-05-04 17:48:57 +0530

    Create strings_operations.cpp

---
+++ 13strings/strings_operations.cpp
@@ -0,0 +1,75 @@
+// String operations
+
+#include <iostream>
+#include <string>
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+    string str;
+
+    // string s1 = "fam";
+    // string s2 = "ily";
+    
+    // s1.append(s2);
+    // cout<<s1<<endl;
+
+    // cout<<s1+s2<<endl;
+
+    // cout<<s1[1]<<endl;
+
+    // string abc = "skshfkdskkf lgdlg lgfhlg l";
+    // abc.clear();
+    // cout<<abc;
+
+    // string s1 = "abc";
+    // string s2 = "abc";
+
+    // if(s1.compare(s2)==0){
+    //     cout<<"Strings are equal"<<endl;
+    // }
+    // cout<<s2.compare(s1)<<endl;
+
+    // string s1 = "abc";
+    // s1.clear();
+    // if(s1.empty())
+    //     cout<<"String is empty";
+
+    // string s1 = "nincompoop";
+    // s1.erase(3, 2);
+    // cout<<s1<<endl;
+
+    // string s1 = "nincompoop";
+    // cout<<s1.find("com")<<endl;
+
+    // string s1 = "nincompoop";
+    // s1.insert(2, "lol");
+    // cout<<s1<<endl;
+    
+    // string s1 = "nincompoop";
+    // cout<<s1.size()<<endl;
+    // cout<<s1.length()<<endl;
+
+    // string s1 = "nincompoop";
+    // for(int i=0;i<s1.size();i++){
+    //     cout<<s1[i]<<endl;
+    // }
+
+    // string s1 = "nincompoop";
+    // string s = s1.substr(6,4);
+    // cout<<s<<endl;
+
+    // string s1 = "786";
+    // int x = stoi(s1);
+    // cout<<x+2;
+
+    // int x = 786;
+    // cout<<to_string(x)+"2";
+
+    // string s1 = "bcda";
+    // sort(s1.begin(), s1.end());
+    // cout<<s1<<endl;
+
+    return 0;
+}

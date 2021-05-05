cfe2cfe5a8ab21fe43cb0124d0c662b6f2c6097f
Prahlad Nayak <pnayak1501@gmail.com>
2021-05-05 18:27:40 +0530

    Create *remove_duplicates.cpp

---
+++ 16recursion/*remove_duplicates.cpp
@@ -0,0 +1,25 @@
+// Remove all duplicates from the string
+
+#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string removeDup (string s){
+    if(s.length() == 0){
+        return "";
+    }
+    char ch = s[0];
+    string ans = removeDup(s.substr(1));
+
+    if(ch==ans[0]){
+        return ans;
+    }
+    return (ch+ans);
+}
+
+int main() {
+
+    cout<<removeDup("aaaabbbbbbbbceee");
+    return 0;
+}

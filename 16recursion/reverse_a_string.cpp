e5d3fb34152742fcbe39fa6fabe9b3d897f9de0e
Prahlad Nayak <pnayak1501@gmail.com>
2021-05-05 10:59:38 +0530

    Create reverse_a_string.cpp

---
+++ 16recursion/reverse_a_string.cpp
@@ -0,0 +1,22 @@
+// Reverse a string
+
+#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void reverse(string s){
+    if(s.size()==0){
+        return;
+    }
+    string ros = s.substr(1);
+    reverse(ros);
+    cout<<s[0];
+}
+
+int main() {
+
+    string s = "binod";
+    reverse(s);
+    return 0;
+}

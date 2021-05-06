398c0271e5fa62e776ba58a6931eddcfd30c8ba5
Prahlad Nayak <pnayak1501@gmail.com>
2021-05-06 16:40:25 +0530

    Create ll_reverse.cpp

---
+++ 20linkedlist/ll_reverse.cpp
@@ -0,0 +1,117 @@
+// LinkedList reverse
+
+#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class node{
+    public: int data;
+    node* next;
+
+    node(int val){
+        data = val;
+        next = NULL;
+    }
+};
+
+void insertAtHead(node* &head, int val){
+
+    node* n = new node(val);
+    n->next = head;
+    head = n;
+
+}
+
+void insertAtTail(node* &head, int val){ // pass by reference as we are modifying the linkedlist
+
+    node* n = new node(val);
+
+    if(head == NULL){
+        head = n;
+        return;
+    }
+
+    node* temp = head;
+    while(temp->next!=NULL){
+        temp = temp->next;
+    }
+    temp->next = n;
+
+}
+
+bool search(node *head, int key){
+    node* temp = head;
+    while(temp!=NULL){
+        if(temp->data == key){
+            return true;
+        }
+        temp = temp->next;
+    }
+    return false;
+}
+
+void display(node* head){ // pass by value as we are not modifying the linkedlist, and we are just accessing the linkedlist
+    node* temp = head;
+    while(temp!=NULL){
+        cout<<temp->data<<"->";
+        temp = temp->next;
+    }
+    cout<<"NULL"<<endl;
+}
+
+void deleteAtHead(node* &head){
+    node* todelete = head;
+    head = head->next;
+    delete todelete;    
+}
+
+
+void deletion(node* &head, int val){
+
+    if(head == NULL){
+        return;
+    }
+    if(head->next == NULL){
+        deleteAtHead(head);
+        return;
+    }
+
+    node* temp = head;
+    while(temp->next->data != val){
+        temp = temp->next;
+    }
+    node* todelete = temp->next;
+    temp->next = temp->next->next;
+    delete todelete;
+
+}
+
+void reverse(node* &head){
+    node* prevptr = NULL;
+    node* currptr = head;
+    node* nextptr = head;
+
+    while(currptr!=NULL){
+        nextptr = currptr->next;
+        currptr->next = prevptr;
+        prevptr = currptr;
+        currptr = nextptr;
+    }
+    head = prevptr;
+}
+
+
+int main() {
+
+    node* head = NULL;
+    insertAtTail(head,1);
+    insertAtTail(head,2);
+    insertAtTail(head,3);
+    insertAtTail(head,4);
+    display(head);
+    reverse(head);
+    display(head);    
+    return 0;
+
+}

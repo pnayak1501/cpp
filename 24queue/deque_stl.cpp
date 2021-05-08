6c518cc2a24f57d10713b68f6ce3a1f7a4deb897
Prahlad Nayak <pnayak1501@gmail.com>
2021-05-08 20:42:37 +0530

    Create deque_stl.cpp

---
+++ 24queue/deque_stl.cpp
@@ -0,0 +1,30 @@
+// Deque STL
+
+#include<iostream>
+#include<deque> 
+
+using namespace std;
+
+int main(){
+    
+    deque<int> dq;
+    dq.push_back(1);
+    dq.push_back(2);
+    dq.push_front(3);
+    dq.push_front(4);
+    cout<<dq.front()<<endl;
+    cout<<dq.back()<<endl;
+    for(auto i : dq){
+        cout<<i<<" ";
+    }
+    cout<<endl;
+    dq.pop_back();
+    dq.pop_front();
+    for(auto i : dq){
+        cout<<i<<" ";
+    }
+    cout<<endl;
+    cout<<dq.size()<<endl;
+    return 0;
+
+}

fa6271c694209fb190924e465ccc7e64fe336ca0
Prahlad Nayak <pnayak1501@gmail.com>
2021-05-09 12:08:47 +0530

    Create sliding_window_maximum_using_vector.cpp

---
+++ 25cp1/sliding_window_maximum_using_vector.cpp
@@ -0,0 +1,22 @@
+// Sliding window maximum
+
+#include<iostream>
+#include<vector> 
+#include<algorithm>
+
+using namespace std;
+
+int main(){
+    int n,k;
+    cin>>n>>k;
+    vector<int> a(n);
+    for(auto &i:a){
+        cin>>i;
+    }
+    for(int i=0;i<=a.size()-k;i++){
+        int m = *max_element(a.begin()+i,a.begin()+i+k);
+        cout<<m<<" ";
+    }
+    return 0;
+
+}

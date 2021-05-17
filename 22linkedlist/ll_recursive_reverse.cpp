ea613e374521ced5289c1b2b61276fe81bd92308
Prahlad Nayak <pnayak1501@gmail.com>
2021-05-17 20:16:19 +0530

    Update ll_recursive_reverse.cpp

--- 22linkedlist/ll_recursive_reverse.cpp
+++ 22linkedlist/ll_recursive_reverse.cpp
@@ -6,6 +6,7 @@
 using namespace std;
 
 class node{
+    
     public: int data;
     node* next;
 
@@ -13,6 +14,7 @@ class node{
         data = val;
         next = NULL;
     }
+    
 };
 
 void insertAtHead(node* &head, int val){

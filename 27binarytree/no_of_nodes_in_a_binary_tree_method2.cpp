beae887e1516e82f075df296a54583f470877ca4
Prahlad Nayak <pnayak1501@gmail.com>
2021-05-24 13:33:17 +0530

    Create no_of_nodes_in_a_binary_tree_method2.cpp

---
+++ 27binarytree/no_of_nodes_in_a_binary_tree_method2.cpp
@@ -0,0 +1,40 @@
+// Count all the nodes in a binary tree
+
+#include<iostream>
+#include "bits/stdc++.h"
+
+using namespace std;
+
+class Node{
+        public:
+        int data;
+        struct Node* left;
+        struct Node* right;
+
+        Node(int val){
+                data = val;
+                left = NULL;
+                right = NULL;
+        }
+};
+
+int countnodes(Node* root){
+	if(root==NULL){
+		return 0;
+	}
+	return countnodes(root->left) + countnodes(root->right) + 1;
+}
+
+int main(){
+
+    struct Node* root = new Node(1);
+    root->left = new Node(2);
+    root->right = new Node(3);
+    root->left->left = new Node(4);
+    root->left->right = new Node(5);
+    root->right->left = new Node(6);
+	root->right->right = new Node(7);
+    cout<<countnodes(root);
+    return 0;
+
+}

04c5fe65eb1e1424f48eed3ca7fb0e4b881e2ac0
Prahlad Nayak <pnayak1501@gmail.com>
2021-05-24 13:16:06 +0530

    Update level_order_traversal.cpp

--- 27binarytree/level_order_traversal.cpp
+++ 27binarytree/level_order_traversal.cpp
@@ -19,30 +19,52 @@ class Node{
 };
 
 void printLevelOrder(Node* root){
-        if(root==NULL){
-                return;
-        }
-        queue<Node*> q;
-        q.push(root);
-        q.push(NULL);
+    if(root==NULL){
+        return;// Level order traversal
 
-        while(!q.empty()){
-                Node* node = q.front();
-                q.pop();
-                if(node != NULL){
-                        cout<<node->data;
-                        if(node->left!=NULL){
-                                q.push(node->left);
-                        }
-                        if(node->right!=NULL){
-                                q.push(node->right);
-                        }
-                }
-                else if(!q.empty()){
-                        q.push(NULL);
-                }
+#include<iostream>
+#include "bits/stdc++.h"
+
+using namespace std;
+
+class Node{
+        public:
+        int data;
+        struct Node* left;
+        struct Node* right;
+
+        Node(int val){
+                data = val;
+                left = NULL;
+                right = NULL;
         }
-        
+};
+
+void printLevelOrder(Node* root){
+    if(root==NULL){
+        return;
+    }
+
+	queue<Node*> q;
+	q.push(root);
+	q.push(NULL);
+
+	while(!q.empty()){
+		Node* node = q.front();
+		q.pop();
+		if(node != NULL){
+			cout<<node->data;
+			if(node->left!=NULL){
+				q.push(node->left);
+			}
+			if(node->right!=NULL){
+				q.push(node->right);
+			}
+		}
+		else if(!q.empty()){
+			q.push(NULL);
+		}
+	}       
 }
 
 int main(){
@@ -53,7 +75,44 @@ int main(){
     root->left->left = new Node(4);
     root->left->right = new Node(5);
     root->right->left = new Node(6);
-    root->right->right = new Node(7);
+	root->right->right = new Node(7);
+    printLevelOrder(root);
+    return 0;
+
+}
+    }
+
+	queue<Node*> q;
+	q.push(root);
+	q.push(NULL);
+
+	while(!q.empty()){
+		Node* node = q.front();
+		q.pop();
+		if(node != NULL){
+			cout<<node->data;
+			if(node->left!=NULL){
+				q.push(node->left);
+			}
+			if(node->right!=NULL){
+				q.push(node->right);
+			}
+		}
+		else if(!q.empty()){
+			q.push(NULL);
+		}
+	}       
+}
+
+int main(){
+
+    struct Node* root = new Node(1);
+    root->left = new Node(2);
+    root->right = new Node(3);
+    root->left->left = new Node(4);
+    root->left->right = new Node(5);
+    root->right->left = new Node(6);
+	root->right->right = new Node(7);
     printLevelOrder(root);
     return 0;
 

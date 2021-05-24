cf5df5ef90f8a2c2a910fabb521329b5ff9a75ec
Prahlad Nayak <pnayak1501@gmail.com>
2021-05-24 13:30:53 +0530

    Update no_of_nodes_in_a_binary_tree.cpp

--- 27binarytree/no_of_nodes_in_a_binary_tree.cpp
+++ 27binarytree/no_of_nodes_in_a_binary_tree.cpp
@@ -54,7 +54,7 @@ int main(){
     root->left->left = new Node(4);
     root->left->right = new Node(5);
     root->right->left = new Node(6);
-	root->right->right = new Node(7);
+    root->right->right = new Node(7);
     countnodes(root);
     return 0;
 

8c2e36fb37d305b9fe0a79ef985027adeb327a01
Prahlad Nayak <pnayak1501@gmail.com>
2021-05-24 13:30:00 +0530

    Create no_of_nodes_in_a_binary_tree.cpp

---
+++ 27binarytree/no_of_nodes_in_a_binary_tree.cpp
@@ -0,0 +1,61 @@
+// Count all the nodes in a binary tree
+
+#include<iostream>
+#include "bits/stdc++.h"
+
+using namespace std;
+
+class Node{
+        public:
+        int data;
+        struct Node* left;
+        struct Node* right;
+
+        Node(int val){
+                data = val;
+                left = NULL;
+                right = NULL;
+        }
+};
+
+void sumatkthlevel(Node* root){
+    if(root==NULL){
+        return;
+    }
+
+	queue<Node*> q;
+	q.push(root);
+	q.push(NULL);
+	int i=0;
+	while(!q.empty()){
+		Node* node = q.front();
+		q.pop();
+		if(node != NULL){
+			i++;
+			if(node->left!=NULL){
+				q.push(node->left);
+			}
+			if(node->right!=NULL){
+				q.push(node->right);
+			}
+		}
+		else if(!q.empty()){
+			q.push(NULL);
+		}
+	}
+	cout<<i;      
+}
+
+int main(){
+
+    struct Node* root = new Node(1);
+    root->left = new Node(2);
+    root->right = new Node(3);
+    root->left->left = new Node(4);
+    root->left->right = new Node(5);
+    root->right->left = new Node(6);
+	root->right->right = new Node(7);
+    sumatkthlevel(root);
+    return 0;
+
+}

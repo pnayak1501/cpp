54d7f8037397bfd1d7662ddfb0e7ac99d8705bee
Prahlad Nayak <pnayak1501@gmail.com>
2021-06-09 00:08:54 +0530

    Create Adjacency_matrix.cpp

---
+++ 34graphs/Adjacency_matrix.cpp
@@ -0,0 +1,28 @@
+// Adjacency matrix
+
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n, m;
+    cin>>n>>m;
+    vector<vector<int>> v( n+1 , vector<int> (n+1, 0));
+    int a;
+    for(int i=0;i<m;i++){
+        int x, y;
+        cin>>x>>y;
+        v[x][y] = 1;
+        v[y][x] = 1;
+    }
+
+    for(int i=1; i<n+1; i++){
+        for(int j=1; j<n+1; j++){
+            cout<<v[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+
+    return 0;
+}
